d9a009e9fd19636ff259b4125a880fc3f6de939b
Hattan Shobokshi <hattan@gmail.com>
2013-07-03 18:01:32 -0700

    hattan cleanup#1

--- app/js/directives.js
+++ app/js/directives.js
@@ -2,9 +2,7 @@
 
 /* Directives */
 
-// add a namespace for custom directives
 angular.module('myApp.directives', []);
-
 angular.module('myApp.directives')
 	.directive('slider', function () {
 		return{
@@ -29,34 +27,3 @@ angular.module('myApp.directives')
 			}
 		}
 	});
-
-angular.module('myApp.directives').directive('myBlinker', function() {
-    return {
-        restrict: 'E',
-        transclude: true,
-        templateUrl: 'views/blink.html',
-        scope: {},
-        link: function(scope, element, attrs)
-        {
-            var marquee = element.find("marquee");
-            var input = element.find("input");
-
-            // Attach blur event
-            input.bind('blur', function()
-            {
-                scope.$apply(function()
-                {
-                    scope.editMode = false;
-                    marquee.text(input.val());
-                });
-            });
-            // edit mode boolean controls the visibility of the blink and input
-            scope.editMode = false;
-            // called when the marquee tag is clicked
-            scope.edit = function() {
-                scope.editMode = true;
-                input.val(marquee.text());
-            };
-        }
-    };
-});
